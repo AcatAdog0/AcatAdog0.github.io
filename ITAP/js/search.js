6dba9dee758ccb03ce4046c119d5b1cf61661cb4
AcatAdog0 <790953820@qq.com>
2019-07-12 22:25:56 +0800

    删除之前的测试文件，修改了网页名字

--- ITAP/js/search.js
+++ ITAP/js/search.js
@@ -2,7 +2,7 @@
 * @Author: 刘炎
 * @Date:   2019-07-10 21:54:43
 * @Last Modified by:   刘炎
-* @Last Modified time: 2019-07-10 22:01:49
+* @Last Modified time: 2019-07-11 11:06:24
 */
 function myFunction() {
   // 声明变量
@@ -11,16 +11,29 @@ function myFunction() {
   filter = input.value.toUpperCase();
   table = document.getElementById("table");
   tr = table.getElementsByTagName("tr");
+  rows = table.rows;
 
   // 循环表格每一行，查找匹配项
   for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[0];
+  	for(j = 0; j < rows[i].cells.length; j++)
+  	{
+  		//td = rows[i].cells[j];
+  	 
+
+  		td = tr[i].getElementsByTagName("td")[0];
+
     if (td) {
       if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
         tr[i].style.display = "";
+
       } else {
         tr[i].style.display = "none";
       }
-    } 
-  }
+    }
+    }
 }
+
+}
+
+
+  
